ec20b2acf09820aa12a41d8808a7c82a6c165615
Ammu <Ammu@LAPTOP-DKIAAQ84>
2022-01-27 16:52:53 +0530

    my first project

---
+++ com.crm.sdet2/src/main/java/mavenCMD/CreateProductTest.java
@@ -0,0 +1,13 @@
+package mavenCMD;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest {
+@Test
+public void createProduct() {
+	System.out.println("creating product");
+}
+	
+	
+
+}
